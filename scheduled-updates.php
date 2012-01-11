12fbe56b2cae43f841ff1538a128e4927e2a105c
Jeff Stieler <jeff.m.stieler@gmail.com>
2012-01-11 12:10:07 -0500

    Version all post meta and taxonomies when supports "future-update".

--- scheduled-updates.php
+++ scheduled-updates.php
@@ -7,12 +7,16 @@
 	Author URI: http://plugins.voceconnect.com
 */
 
+require('meta-revisions/meta-revisions.php');
+
 class Scheduled_Updates {
 
 	const POST_STATUS = 'future-update';
 
 	public static function initialize() {
 		add_action('init', array(__CLASS__, 'create_future_update_status'), 1);
+		add_action('Meta_Revisions_init', array(__CLASS__, 'setup_term_revisions'));
+		add_action('pre_version_meta', array(__CLASS__, 'setup_meta_revisions'));
 	}
 
 	public static function create_future_update_status() {
@@ -26,6 +30,30 @@ class Scheduled_Updates {
 				'show_in_admin_status_list' => true
 			)
 		);
+	}
+
+	public static function setup_term_revisions() {
+		$post_types = get_post_types();
+		foreach ($post_types as $post_type) {
+			if (post_type_supports($post_type, self::POST_STATUS)) {
+				$taxonomies = get_object_taxonomies($post_type, 'objects');
+				foreach ($taxonomies as $taxonomy_obj) {
+					error_log('tracking taxonomy: ' . $taxonomy_obj->name . ' for post type: ' . $post_type);
+					meta_revisions_track_taxonomy_field($taxonomy_obj->name, $taxonomy_obj->labels->name, $post_type);
+				}
+			}
+		}
+	}
+
+	public static function setup_meta_revisions($post_id) {
+		$post_type = get_post_type($post_id);
+		if (post_type_supports($post_type, self::POST_STATUS)) {
+			$meta_keys = get_post_custom_keys($post_id);
+			foreach ($meta_keys as $meta_key) {
+				error_log('tracking meta key: ' . $meta_key . ' for post type: ' . $post_type);
+				meta_revisions_track_postmeta_field($meta_key, $meta_key, $post_type);
+			}
+		}
 	}
 
 }
